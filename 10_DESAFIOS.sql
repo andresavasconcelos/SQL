431b27aca2d96302a41482ca1e19992f4c8173ba
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2020-09-15 23:18:39 -0300

    new class

---
+++ 10_DESAFIOS.sql
@@ -0,0 +1,10 @@
+-- 1 Quantos produtos que temos cadastrado no sistema que custam mais de 1500?
+
+-- 2 Quantos nomes de pessoas que temos com a inicial P?
+
+-- 3 Quantas cidades unicas estao cadastradas em nosso sistema?
+
+-- 5 Quantos produtos vermelhos tem preço entre 500 a 100 dolares?
+
+-- 6 Quantos produtos cadastratos tem a palavra road no nome deles?
+
